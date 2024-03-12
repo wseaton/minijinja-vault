6bda95815c8223ba6242b51dc19fc992a0aa7761
Will Eaton <me@wseaton.com>
2024-03-12 10:05:59 -0400

    add environment variables to config

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
 #![allow(clippy::let_unit_value)]
+
 use std::fmt;
 
-use minijinja::value::{from_args, Object, Value};
+use minijinja::value::{from_args, Kwargs, Object, Value};
 use minijinja::{Environment, Error, State};
 
+use vaultrs::auth::approle::role::{self, secret};
 use vaultrs::client::{VaultClient, VaultClientSettingsBuilderError};
 use vaultrs::kv2;
 
@@ -62,40 +64,50 @@ impl MinijinjaVaultClient {
 }
 
 pub fn make_vault_client(_state: &State, args: Vec<Value>) -> Result<Value, Error> {
-    let client = match args.as_slice() {
-        [addr, role_id, secret_id] => {
-            let mut settings = vaultrs::client::VaultClientSettingsBuilder::default();
-            settings.address(addr.to_string());
-            settings.verify(false);
+    let (_, kwargs): (&[Value], Kwargs) = from_args(&args)?;
 
-            let the_settings = settings
-                .build()
-                .map_err(VaultClientSettingsBuilderError::from)
-                .expect("failed to build settings");
+    let addr: Option<&str> = kwargs.get("address")?;
+    let addr = addr
+        .map(|s| s.to_string())
+        .unwrap_or_else(|| std::env::var("VAULT_ADDR").expect("VAULT_ADDR not set"));
 
-            // Use one of the login flows to obtain a token for the client
-            let login = AppRoleLogin {
-                role_id: role_id.to_string(),
-                secret_id: secret_id.to_string(),
-            };
+    let role_id: Option<&str> = kwargs.get("role_id")?;
+    let role_id = role_id
+        .map(|s| s.to_string())
+        .unwrap_or_else(|| std::env::var("VAULT_ROLE_ID").expect("VAULT_ROLE_ID not set"));
 
-            let rt = tokio::runtime::Runtime::new().expect("failed to create runtime");
-            let mut client = VaultClient::new(the_settings).expect("failed to create client");
+    let secret_id: Option<&str> = kwargs.get("secret_id")?;
+    let secret_id = secret_id
+        .map(|s| s.to_string())
+        .unwrap_or_else(|| std::env::var("VAULT_SECRET_ID").expect("VAULT_SECRET_ID not set"));
 
-            rt.block_on(client.login("approle", &login))
-                .expect("fauled to login");
+    let verify: Option<bool> = kwargs.get("verify")?;
+    let verify = verify.unwrap_or_else(|| {
+        std::env::var("VAULT_SKIP_VERIFY")
+            .map(|s| s == "false")
+            .unwrap_or(true)
+    });
 
-            client
-        }
-        _ => {
-            return Err(Error::new(
-                minijinja::ErrorKind::MissingArgument,
-                "expected (str, str)",
-            ))
-        }
+    let mut settings = vaultrs::client::VaultClientSettingsBuilder::default();
+    settings.address(addr);
+    settings.verify(verify);
+
+    let the_settings = settings
+        .build()
+        .map_err(VaultClientSettingsBuilderError::from)
+        .expect("failed to build settings");
+
+    // Use one of the login flows to obtain a token for the client
+    let login = AppRoleLogin {
+        role_id: role_id.to_string(),
+        secret_id: secret_id.to_string(),
     };
+
+    let rt = tokio::runtime::Runtime::new().expect("failed to create runtime");
+    let mut client = VaultClient::new(the_settings).expect("failed to create client");
+
+    rt.block_on(client.login("approle", &login))
+        .expect("fauled to login");
 
     Ok(Value::from_object(MinijinjaVaultClient(client)))
 }
-
-
